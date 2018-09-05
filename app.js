7c66ded936db742c7aca32576181c7d46266565e
Fabrício Lago <fabricio@hipcom.com.br>
2018-09-05 10:25:11 -0300

    inserção do mongo

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const AssistantV1 = require('watson-developer-cloud/assistant/v1');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const Question = require('./model/question');
+const contexto = require('./config/context');
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,10 +12,10 @@ app.use(express.static('./public'));
 const port = 3000;
 
 const assistant = new AssistantV1({
-  username: 'b4d74b7f-8e70-494d-8658-bac48fb91f82',
-  password: 'yB4JXPOAvIP7',
-  url: 'https://gateway.watsonplatform.net/assistant/api/',
-  version: '2018-02-16',
+  username: contexto.watson.username,
+  password: contexto.watson.password,
+  url: contexto.watson.url,
+  version: contexto.watson.version
 });
 
 app.post('/conversation/', (req, res) => {
@@ -22,12 +23,19 @@ app.post('/conversation/', (req, res) => {
 
   const params = {
     input: { text },
-    workspace_id: '6fa21cad-09c7-4910-b34d-d02275796785',
+    workspace_id: contexto.watson.workspace_id,
     context,
   };
 
   assistant.message(params, (err, response) => {
     if (err) res.status(500).json(err);
+
+    Question.create(response, (error, success) => {
+      if (error)
+        console.log("Erro: " + error);
+      else
+        console.log("Sucesso: " + success);
+    });
 
     res.json(response);
   });
